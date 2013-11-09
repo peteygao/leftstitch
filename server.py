df290b9b2bc41ddaf2f7fd3940584f8db22fa42e
Peter Gao <pgao@quantcast.com>
2013-11-09 00:08:43 -0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,7 @@
+from bottle import route, run, static_file
+
+@route('/<filename:path>')
+def server_static(filename):
+        return static_file(filename, root='.')
+
+run(host="0.0.0.0", port=8080)
