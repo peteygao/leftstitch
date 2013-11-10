8bb5afb7166767f5bd520806b5bb8caf7b6afe8a
awongh <akira@awongh.com>
2013-11-09 16:30:20 -0800

    changes for biref entry

--- js/angular-app/modules/app.js
+++ js/angular-app/modules/app.js
@@ -1,9 +1,11 @@
 "use strict";
 
 angular.module('leftstitch', ['ui.bootstrap'])
-    .controller('chatController', ['$scope', chatController])
+    //.controller('chatController', ['$scope', chatController])
+    .controller('briefEntryController', ['$scope', briefEntryController])
     .controller('mainController', ['$scope', mainController])
     .run(function($rootScope) {
         $rootScope._content = new Firebase('https://leftstitch.firebaseio.com/content');
-        $rootScope._form = new Firebase('https://leftstitch.firebaseio.com/form');
+        $rootScope._briefs = new Firebase('https://leftstitch.firebaseio.com/briefs');
+        console.log( $rootScope._briefs );
     });
