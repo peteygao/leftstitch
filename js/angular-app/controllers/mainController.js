cbe6cb161f671d87578167b63e7af67feaa68bc3
Peter Gao <pgao@quantcast.com>
2013-11-09 20:43:36 -0800

    Add placeholder images. Add boilerplate bootstrap classes.

--- js/angular-app/controllers/mainController.js
+++ js/angular-app/controllers/mainController.js
@@ -2,13 +2,16 @@
 
 var mainController = function($scope) {
     $scope.pictures = {
-        1: '//placehold.it/250x250',
-        2: '//placehold.it/250x250',
-        3: '//placehold.it/250x250',
-        4: '//placehold.it/250x250',
-        5: '//placehold.it/250x250',
-        6: '//placehold.it/250x250',
-        7: '//placehold.it/250x250'
+        1: 'img/3392063_offing.jpg',
+        2: 'img/3392437_asukollaasi13.jpg',
+        3: 'img/3392450_IMG_5911111109.jpg',
+        4: 'img/3392487_DSC02725.JPG_effected_Kopie.jpg',
+        5: 'img/3392503_lb3.jpg',
+        6: 'img/this-girl-knows-her-prereqs.jpg',
+        7: 'img/3392968_loook.jpg',
+        8: 'img/fashion_sketch__floral_by_Inese.jpg',
+        9: 'img/3392667_Sem_tiitulo15.jpg',
+        10: 'img/photo-8.jpg'
     }
 
     $scope.init = function() {
