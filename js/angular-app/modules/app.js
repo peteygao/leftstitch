4c53aaabbbe79528ef40aa8c52e4fecdb02a826c
Peter Gao <pgao@quantcast.com>
2013-11-10 14:08:47 -0800

    Add file dragging and dropping handling.

--- js/angular-app/modules/app.js
+++ js/angular-app/modules/app.js
@@ -1,10 +1,11 @@
 "use strict";
 
-angular.module('leftstitch', ['ui.bootstrap', 'wu.masonry'])
+angular.module('leftstitch', ['ui.bootstrap', 'wu.masonry', 'reusableThings'])
     .controller('briefController', ['$scope', '$location', briefController])
     .controller('mainController', ['$scope', mainController])
     .run(function($rootScope) {
         $rootScope._content = new Firebase('https://leftstitch.firebaseio.com/content');
         $rootScope._briefs = new Firebase('https://leftstitch.firebaseio.com/briefs');
+        $rootScope._designs = new Firebase('https://leftstitch.firebaseio.com/designs');
         console.log( $rootScope._briefs );
     });
